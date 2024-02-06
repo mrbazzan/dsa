f48a5b43b3b2b120b42406825b021c06ba67a240
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-06 23:28:08 +0100

    ayulla: Request Parser

---
+++ request_parser.py
@@ -0,0 +1,12 @@
+
+##################
+# REQUEST PARSER #
+##################
+
+valid_auth_tokens = ["ah37j2ha483u", "safh34ywb0p5", "ba34wyi8t902"]
+requests = [
+    ["GET", "https://example.com/?token=347sd6yk8iu2&name=alex"],
+    ["GET", "https://example.com/?token=safh34ywb0p5&name=sam"],
+    ["POST", "https://example.com/?token=safh34ywb0p5&name=alex"],
+    ["POST", "https://example.com/?token=safh34ywb0p5&csrf=ak2sh32dy&name=chris"],
+]

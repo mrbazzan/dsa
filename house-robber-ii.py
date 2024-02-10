9759991c35c133e360c2b5778b0ecfa3c4845ef4
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-10 22:43:32 +0100

    dsa: house robber II (suboptimal)

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,33 +1,36 @@
 
+
 def rob(nums):
     arr = [0 for _ in range(len(nums))]
     arr[0] = nums[0]
 
-    for i, num in enumerate(nums[1:]):
-        two_steps_back = arr[i-1] if i >= 1 else 0
-        # print(i, two_steps_back, num, arr[i])
+    for i in range(1, len(nums)):
+        two_step = arr[i-2] if i >= 2 else 0
+        arr[i] = max(nums[i] + two_step, arr[i-1])
 
-        answer = two_steps_back + num
-
-        if (i+2 != len(nums)):
-            arr[i+1] = max(((arr[i-1] if i >= 0 else 0) + num), arr[i])
-        else:
-            arr[i+1] = max(((nums[i-1] if i > 1 else 0) + num), arr[i])
-
-    return arr
+    return arr[-1]
 
 
-print(rob([2,3,2]))
-print(rob([1,2,3,1]))
-print(rob([3,2,6,6,4]))
-print(rob([3,2,7,6]))
+def circular_rob(nums):
+    length = len(nums) - 1
 
+    if length <= 2:
+        return max(nums)
 
-# print(rob([1,2,2,4,1]))
-# print(rob([1,2,2,2,1]))
-# print("---------: ", rob([1,2,3,1]))
-# print("---------: ", rob([2,1,1,2]))
-# print("---------: ", rob([1,2,7,8,4,5]))
-# print("---------: ", rob([2,7,9,3,1]))
+    return max(
+        nums[0]+rob(nums[2:length]),
+        nums[-1]+rob(nums[1:length-1]),
+        nums[-2]+rob(nums[:length-2]),
+    )
+
+print(circular_rob([2,3,2]))
+print(circular_rob([4,2,3]))
+print(circular_rob([1,2,3,1]))
+print(circular_rob([3,2,6,6,4]))
+print(circular_rob([3,2,7,6]))
+print(circular_rob([4,1,2,1,3]))
+print(circular_rob([3,1,2,1,4]))
+print(circular_rob([2,7,7,7,4]))
+print(circular_rob([4,2,3,7,4,3,3,4]))
 
 

8387c222a455d44a89c5dbc6d35b41480464e6a3
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-10 23:06:22 +0100

    house robber II complete

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,28 +1,23 @@
 
 
 def rob(nums):
-    arr = [0 for _ in range(len(nums))]
-    arr[0] = nums[0]
+    rob0, rob1 = 0, 0
+    for num in nums:
+        new_rob = max(rob0 + num, rob1)
+        rob0 = rob1
+        rob1 = new_rob
 
-    for i in range(1, len(nums)):
-        two_step = arr[i-2] if i >= 2 else 0
-        arr[i] = max(nums[i] + two_step, arr[i-1])
-
-    return arr[-1]
-
+    return rob1
 
 def circular_rob(nums):
-    length = len(nums) - 1
-
-    if length <= 2:
-        return max(nums)
-
     return max(
-        nums[0]+rob(nums[2:length]),
-        nums[-1]+rob(nums[1:length-1]),
-        nums[-2]+rob(nums[:length-2]),
+        nums[0],
+        rob(nums[:-1]),
+        rob(nums[1:]),
     )
 
+print(circular_rob([4]))
+print(circular_rob([4,2]))
 print(circular_rob([2,3,2]))
 print(circular_rob([4,2,3]))
 print(circular_rob([1,2,3,1]))
@@ -32,5 +27,3 @@ print(circular_rob([4,1,2,1,3]))
 print(circular_rob([3,1,2,1,4]))
 print(circular_rob([2,7,7,7,4]))
 print(circular_rob([4,2,3,7,4,3,3,4]))
-
-

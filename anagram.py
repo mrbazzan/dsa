4add0f14ab6d9813589eae1a2ac89ee57b519312
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-11 17:03:59 +0100

    dsa: anagram(full solution)

--- anagram.py
+++ anagram.py
@@ -1,8 +1,23 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
 
+        # make a dictionary of the character in each words
+        # and their counts
+        dict_s, dict_t = {}, {}
+        for i in range(len(s)):
+            dict_s[s[i]] = 1 + dict_s.get(s[i], 0)
+            dict_t[t[i]] = 1 + dict_t.get(t[i], 0)
+
+        for char, count in dict_t.items():
+            if dict_s.get(char) != count:
+                return False
+
+        return True
+
+print(Solution().isAnagram("anagram", "abangrm"))
 print(Solution().isAnagram("anagram", "aaangrm"))
 print(Solution().isAnagram("cat", "rat"))
 print(Solution().isAnagram("ab", "a"))

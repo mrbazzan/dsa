ef6fce45fccf90e91ef50d125e5a3ec760b634bb
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-13 21:29:19 +0100

    two sum o(n)

--- two_sum.py
+++ two_sum.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for i in range(len(nums)):
-            remainder = nums[i+1:]
             diff = target - nums[i]
-            if diff in remainder:
-                return [i, i+1+remainder.index(diff) if diff==nums[i] else nums.index(diff)]
-
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[nums[i]] = i
 
 print(Solution().twoSum([2,7,11,15], 9))
 print(Solution().twoSum([3, 2, 4], 6))

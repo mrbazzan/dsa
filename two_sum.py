c5a5b74f7af5c214ec4d55654be97f70c3f1fd13
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-13 19:38:34 +0100

    two sum o(n**2)

---
+++ two_sum.py
@@ -0,0 +1,16 @@
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+print(Solution().twoSum([2,7,11,15], 9))
+print(Solution().twoSum([3, 2, 4], 6))
+print(Solution().twoSum([3, 3], 6))
+print(Solution().twoSum([-1, 2, 8, 10, 13, 15], 14))
+
+

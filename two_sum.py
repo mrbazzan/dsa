28541b4249a2036e044cabb1e6e994b6f4f96cf4
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-13 21:21:40 +0100

    two_sum [faster o(n**2)]

--- two_sum.py
+++ two_sum.py
@@ -3,14 +3,16 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        for i in range(len(nums)):
+            remainder = nums[i+1:]
+            diff = target - nums[i]
+            if diff in remainder:
+                return [i, i+1+remainder.index(diff) if diff==nums[i] else nums.index(diff)]
+
 
 print(Solution().twoSum([2,7,11,15], 9))
 print(Solution().twoSum([3, 2, 4], 6))
 print(Solution().twoSum([3, 3], 6))
-print(Solution().twoSum([-1, 2, 8, 10, 13, 15], 14))
+print(Solution().twoSum([2, 5, 11, 11, 5], 10))
 
 

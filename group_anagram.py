15c753982d6e064e810003646e84d46a629df56f
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-14 23:06:16 +0100

    group anagram -> o(m*n) family
    The first one takes into account the sort time while the second one uses
    an array.

--- group_anagram.py
+++ group_anagram.py
@@ -3,17 +3,38 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = [[strs[0]]]
-        for word in strs[1:]:
-            for group in groups:
-                if sorted(word) == sorted(group[0]):
-                    group.append(word)
-                    break
+
+        new_str = sorted(strs, key=lambda x: sorted(x))
+
+        first_word = new_str[0]
+        groups = [[]]
+        for word in new_str:
+            if sorted(word) == sorted(first_word):
+                groups[-1].append(word)
             else:
                 groups.append([word])
+                first_word = word
 
         return groups
 
+        # from collections import defaultdict
+        # w = defaultdict(list)
+        # for s in strs:
+        #     w[''.join(sorted(s))].append(s)
+
+        # return list(w.values())
+
+        # OR
+
+        # result = defaultdict(list)
+        # for word in strs:
+        #     count = [0] * 26
+        #     for c in word:
+        #         count[ord(c) - ord("a")] += 1
+
+        #     result[tuple(count)].append(word)
+
+        # return list(result.values())
 
 print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 print(Solution().groupAnagrams(["a"]))

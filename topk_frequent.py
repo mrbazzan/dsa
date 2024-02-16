68aef967292024da814240cc76e4313e0af8e43d
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-16 03:06:46 +0100

    top k requent element -> o(n)

--- topk_frequent.py
+++ topk_frequent.py
@@ -3,13 +3,26 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # get the count of each element
         frequent = {}
         for num in nums:
             frequent[num] = 1 + frequent.get(num, 0)
 
-        return sorted(frequent, key=lambda x: frequent[x], reverse=True)[:k]
+        # make a bucket sort with the count as key
+        bucket = [[] for i in range(len(nums)+1)]
+        for num, count in frequent.items():
+            bucket[count].append(num)
+
+        # generate the result
+        result = []
+        for i in range(len(bucket)-1, 0, -1):
+            for element in bucket[i]:
+                result.append(element)
+                if len(result) == k:
+                    return result
+
 
 print(Solution().topKFrequent([1,1,1,2,2,3],2))
 print(Solution().topKFrequent([1],1))
-print(Solution().topKFrequent([1,2,1,2,2,1,1,1,2,4,3,4,3,3],3))
+print(Solution().topKFrequent([2,1,1,8,8,2,2,1,1,1,2,4,3,4,3,3],4))
 

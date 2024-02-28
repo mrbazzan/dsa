eb6a7ba118890e0d47ccffba91e44fda25545f7d
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-02-28 23:52:45 +0100

    containsDuplicate: solution II

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -9,3 +9,17 @@ class Solution:
 print(Solution().containsDuplicate([1,2,3,4]))
 print(Solution().containsDuplicate([1,2,3,1]))
 
+
+def contains(arr):
+    hash_set = set()
+    for num in arr:
+        if num in hash_set:
+            return True
+        hash_set.add(num)
+
+
+    return False
+
+print(contains([1,2,3,4]))
+print(contains([1,2,3,1]))
+

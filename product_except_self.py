080c27238337d9ade56d934b63a4b4c86a63ff1f
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-03-01 04:14:03 +0100

    product-except-self: 0(n**2)

--- product_except_self.py
+++ product_except_self.py
@@ -2,5 +2,19 @@
 from typing import List
 
 class Solution:
+
+    def product(self, nums):
+        total = 1
+        for i in nums:
+            total *= i
+        return total
+
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        answer = []
+        for i in range(len(nums)):
+            answer.append(self.product(nums[:i] + nums[i+1:]))
+
+        return answer
+
+print(Solution().productExceptSelf([1,2,3,4]))
+print(Solution().productExceptSelf([-1, 1, 0, -3, 3]))

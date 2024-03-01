da32a3663b3fc89ec2f559417d9b0edde14736a9
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-03-01 12:03:19 +0100

    product except self: o(n) time & o(n) space

--- product_except_self.py
+++ product_except_self.py
@@ -2,19 +2,22 @@
 from typing import List
 
 class Solution:
-
-    def product(self, nums):
-        total = 1
-        for i in nums:
-            total *= i
-        return total
-
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer = []
-        for i in range(len(nums)):
-            answer.append(self.product(nums[:i] + nums[i+1:]))
+        answer = [1] * len(nums)
+
+        # sweep through the array to calculate the
+        # prefix product for each index
+        for i in range(1, len(nums)):
+            answer[i] = nums[i-1] * answer[i-1]
+
+        # reverse through the array
+        post_product = 1
+        for i in range(len(nums)-2, -1, -1):
+            post_product *= nums[i+1]
+            answer[i] *= post_product
 
         return answer
 
 print(Solution().productExceptSelf([1,2,3,4]))
 print(Solution().productExceptSelf([-1, 1, 0, -3, 3]))
+

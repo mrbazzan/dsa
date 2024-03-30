01d28adc972cb39528a6007d3db0bc4264766c5b
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-03-30 11:41:41 +0100

    3sum: using two pointer approach

--- 3sum.py
+++ 3sum.py
@@ -1,30 +1,39 @@
 
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        target_dict = {nums[i]: i for i in range(len(nums))}
-        x, y, length = 0, 0, len(nums)
-        triplets = set()
-        while x <= length-3:
-            y = y + 1
-            if y == length:
-                x, y = x + 1, x + 1
+        arr = sorted(nums)
+        triplets = []
+        previous = float('-inf')
+        for i in range(len(arr)):
+            left, right = i+1, len(arr)-1
+            target = 0 - arr[i]
 
-            required = 0 - nums[x] - nums[y]
-            if required not in target_dict:
-                continue
+            while (arr[i] != previous) and left < right:
+                total = arr[left] + arr[right]
 
-            z = target_dict[required]
-            if x != y and x != z and y != z:
-                triplets.add(tuple(sorted((nums[x], nums[y], required))))
+                if total > target:
+                    right = right - 1
+
+                elif total < target: # [-4, -1, -1, 0, 1, 2]
+                    left = left + 1
+
+                else:
+                    triplets.append([arr[i], arr[left], arr[right]])
+
+                    # move forward if the number is same
+                    left = left + 1
+                    while left < right and arr[left] == arr[left-1]:
+                        left = left + 1
+
+            previous = arr[i]
 
         return triplets
 
 
 print(Solution().threeSum([-1,0,1,2,-1,-4]))
-# print(Solution().threeSum([0,1,1]))
-# print(Solution().threeSum([0,0,0]))
+print(Solution().threeSum([0,1,1]))
+print(Solution().threeSum([34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]))
 

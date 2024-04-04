24b608bacd39cf8b096fb7441d85cf88c8f1413d
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-04-04 21:28:56 +0100

    max area of water: better o(n)

--- container_water.py
+++ container_water.py
@@ -4,20 +4,16 @@ from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         max_total = 0
-        length = len(height)
+        left, right = 0, len(height)-1
 
-        for i in range(length):
-            left, right = 0, length-1
-            while height[i] > height[right]:
-                right = right - 1
+        while left < right:
+            area = min(height[left], height[right]) * (right-left)
+            max_total = max(max_total, area)
 
-            while height[length-1-i] > height[left]:
+            if height[left] <= height[right]:
                 left = left + 1
-
-            max_total = max(max_total,
-                            height[i]*(right-i),
-                            height[length-1-i] * (length-1-i-left)
-                        )
+            else:
+                right = right - 1
 
         return max_total
 

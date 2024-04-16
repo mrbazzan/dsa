4023eb147cdc0e7e957ad24b37aa58be130c2b7c
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-04-16 15:14:50 +0100

    predict stock: 0(n**2)

--- buy_and_sell_stock.py
+++ buy_and_sell_stock.py
@@ -3,14 +3,13 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_price = 0
+        profit = 0
 
         for i in range(len(prices)-1):
-            for j in range(i+1, len(prices)):
-                if prices[j] > prices[i]:
-                    max_price = max(max_price, prices[j] - prices[i])
+            max_price = max(prices[i+1:])
+            profit = max(profit, max_price-prices[i])
 
-        return max_price
+        return profit
 
 print(Solution().maxProfit([7,1,5,3,6,4]))  # 7 4 5 3 1 6  # 6 7 4 5 2 1 8 5
 print(Solution().maxProfit([2,1,4]))

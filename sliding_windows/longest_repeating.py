6adc8aba870322ecd4e4f73b28361016c83d7bf5
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-06-19 08:31:28 +0100

    ..contd

--- sliding_windows/longest_repeating.py
+++ sliding_windows/longest_repeating.py
@@ -11,18 +11,18 @@ class Solution:
                 hash_map[char] = 1
         return hash_map
 
-    def char_to_change(self, hash_map: dict) -> str:
+    def freq_char(self, hash_map: dict) -> str:
         count = 0
-        to_change = ''
-        for char in hash_map:
-            if hash_map[char] > count:
-                to_change = char
-                count = hash_map[char]
-        return to_change
+        char = ''
+        for c in hash_map:
+            if hash_map[c] > count:
+                char = c
+                count = hash_map[c]
+        return char
 
     def characterReplacement(self, s: str, k: int) -> int:
         hash_map = self.get_map(s)
-        to_change = self.char_to_change(hash_map)
+        frequent_char = self.freq_char(hash_map)
 
         output = 0
         left, right = 0, 0
@@ -30,32 +30,33 @@ class Solution:
         while left < len(s):
 
             count = 0
-            while right < len(s):
-                if s[right] != to_change: count += 1
-                if count == k:
-                    cont.append(s[left:right+1])
-                    output = max(output, len(s[left:right+1]))
-                right+=1
+            while (count <= k) and right < len(s):
+                if s[right] != frequent_char: count+=1
+                right += 1
+
+            cont.append(s[left:right])
+            output = max(output, len(s[left:right]))
 
             left = left + 1
             right = left
 
-        print(cont)
+        print(cont, frequent_char)
         return output
 
 
 # print(Solution().characterReplacement("XYYX", 0) == 2)
 # print(Solution().characterReplacement("XYYX", 2) == 4)
 # print(Solution().characterReplacement("XYYXX", 2) == 5)
-
 # print(Solution().characterReplacement("XYYXY", 2) == 5)
 # print(Solution().characterReplacement("XYYXYXY", 2) == 6)
 
 print(Solution().characterReplacement("XYYXYX", 2) == 5)
 
 # print(Solution().characterReplacement("XYYYX", 2) == 5)
-# print(Solution().characterReplacement("AAABABB", 1) == 5)
-# print(Solution().characterReplacement("AAABABB", 2) == 6)
+
+print(Solution().characterReplacement("AAABABB", 1) == 5)
+print(Solution().characterReplacement("AAABABB", 2) == 6)
+
 # print(Solution().characterReplacement("AABBABB", 1) == 5)
 # print(Solution().characterReplacement("CABBABB", 1) == 5)
 
